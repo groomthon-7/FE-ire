dec21b3c73be3a67c24ee38750efb5aa51ff36ea
LJH <049494@ewhain.net>
2023-09-07 03:00:48 +0900

    📁 api 폴더 세팅

---
+++ src/api/client.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { SPRING_URL } from "./url";
+
+const client = axios.create();
+client.defaults.baseURL = `${SPRING_URL}`;
+client.defaults.withCredentials = true;
+
+export default client;
---
+++ src/api/url.js
@@ -0,0 +1,1 @@
+export const SPRING_URL = "";
