3a8306f79e87a72ff4ae354a0402327d3175bcd9
y00eunji <aine0227@naver.com>
2023-09-08 08:14:55 +0900

    api 경로 수정

--- IRE/src/api/getCampingList.js
+++ IRE/src/api/getCampingList.js
@@ -2,7 +2,7 @@ import client from './client';
 
 export const getAllCampingApi = () => {
   try {
-    const res = client.get('/api/ques/all');
+    const res = client.get('/ques/all');
     return res.data;
   } catch (err) {
     throw err;
