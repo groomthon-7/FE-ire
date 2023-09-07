e4e04d3a31c4cbbf9a0b3c7d715a0388c9c16bae
유은지 <aine0227@naver.com>
2023-09-07 21:09:08 +0900

    캠핑장리스트 마크업  및 스타일링 구현 (#3)
    * style: 캠핑장리스트 마크업 및 스타일링
    
    * 📝 서버 테스트
    
    ---------
    
    Co-authored-by: LJH <049494@ewhain.net>

--- IRE/src/App.js
+++ IRE/src/App.js
@@ -1,23 +1,27 @@
-import "./App.css";
-import { ThemeProvider } from "styled-components";
-import theme from "./style/theme";
+import './App.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './style/theme';
+import { Routes, Route } from 'react-router-dom';
 
-import { Routes, Route } from "react-router-dom";
-import Test from "./test";
+import CampingList from './pages/CampingList';
 
-const staticServerUrl = process.env.REACT_APP_PATH || "";
+const staticServerUrl = process.env.REACT_APP_PATH || '';
+
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Routes>
-          <Route path={staticServerUrl + "/"} element={<Test />} />
-          <Route path={staticServerUrl + "/research"} />
-          <Route path={staticServerUrl + "/campingList/all"} />
-          <Route path={staticServerUrl + "/campingList/jeju"} />
-          <Route path={staticServerUrl + "/campingList/seongwipo"} />
-          <Route path={staticServerUrl + "/campingDetail/:id"} />
+          <Route path={staticServerUrl + '/'} />
+          <Route path={staticServerUrl + '/research'} />
+          <Route
+            path={staticServerUrl + '/campingList/all'}
+            element={<CampingList />}
+          />
+          <Route path={staticServerUrl + '/campingList/jeju'} />
+          <Route path={staticServerUrl + '/campingList/seongwipo'} />
+          <Route path={staticServerUrl + '/campingDetail/:id'} />
         </Routes>
       </div>
     </ThemeProvider>
---
+++ IRE/src/pages/CampingList.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import Button from '../components/CampingList/Button';
+import CampingCard from '../components/CampingList/Card';
+
+import icon from '../assets/CampingList/Ellipse18.png';
+import { useState } from 'react';
+
+const CampingList = ({ category }) => {
+  const [activeButton, setActiveButton] = useState(0);
+
+  const handleButtonClick = (buttonIndex) => {
+    setActiveButton(buttonIndex);
+  };
+
+  return (
+    <CampingListLayout>
+      <Header>
+        <ButtonContainer>
+          <Button
+            text='전체'
+            active={activeButton === 0 ? 'true' : 'false'}
+            onClick={() => handleButtonClick(0)}
+          />
+          <Button
+            text='제주시'
+            active={activeButton === 1 ? 'true' : 'false'}
+            onClick={() => handleButtonClick(1)}
+          />
+          <Button
+            text='서귀포시'
+            active={activeButton === 2 ? 'true' : 'false'}
+            onClick={() => handleButtonClick(2)}
+          />
+        </ButtonContainer>
+        <MapIcon src={icon} />
+      </Header>
+
+      <CampingListContainer>
+        <CampingCard />
+        <CampingCard />
+        <CampingCard />
+      </CampingListContainer>
+    </CampingListLayout>
+  );
+};
+
+const CampingListLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.875rem;
+  width: 100%;
+
+  margin-bottom: 5rem;
+`;
+
+const CampingListContainer = styled.div`
+  margin-top: 1.25rem;
+`;
+
+const MapIcon = styled.img`
+  height: 1.8125rem;
+  width: 1.8125rem;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+export default CampingList;
