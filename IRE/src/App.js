e75bf489725cea1614aa6a506187dbe4e6bdad75
y00eunji <aine0227@naver.com>
2023-09-07 20:56:01 +0900

    style: 캠핑장리스트 마크업 및 스타일링

--- IRE/src/App.js
+++ IRE/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { ThemeProvider } from 'styled-components';
 import theme from './style/theme';
-
 import { Routes, Route } from 'react-router-dom';
+
+import CampingList from './pages/CampingList';
 
 const staticServerUrl = process.env.REACT_APP_PATH || '';
 
@@ -13,7 +14,10 @@ function App() {
         <Routes>
           <Route path={staticServerUrl + '/'} />
           <Route path={staticServerUrl + '/research'} />
-          <Route path={staticServerUrl + '/campingList/all'} />
+          <Route
+            path={staticServerUrl + '/campingList/all'}
+            element={<CampingList />}
+          />
           <Route path={staticServerUrl + '/campingList/jeju'} />
           <Route path={staticServerUrl + '/campingList/seongwipo'} />
           <Route path={staticServerUrl + '/campingDetail/:id'} />
--- IRE/src/style/theme.js
+++ IRE/src/style/theme.js
@@ -1,6 +1,11 @@
 const fontSize = {};
 const fontWeight = {};
-const color = {};
+const color = {
+  lightGray: '#E1E1E1',
+  darkGray: '#8D8D8D',
+  semiGray: '#EDEDED',
+  white: '#FFFFFF',
+};
 const margin = {};
 
 const theme = {
