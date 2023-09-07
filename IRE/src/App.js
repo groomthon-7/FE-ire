11cf8800fc1f4b146b25d3bf06b1023a338e4b01
LJH <049494@ewhain.net>
2023-09-07 15:15:33 +0900

    📝 설문 데이터 작성

--- IRE/src/App.js
+++ IRE/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">어 잠시만???</div>;
+  return (
+    <Routes>
+      <Route path="/" />
+      <Route path="/research" />
+      <Route path="/campingList/all" />
+      <Route path="/campingList/jeju" />
+      <Route path="/campingList/seongwipo" />
+      <Route path="/campingDetail/:id" />
+    </Routes>
+  );
 }
 
 export default App;
