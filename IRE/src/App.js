132da32fee847302b2ce61856b93cbd8b374d4a5
LJH <049494@ewhain.net>
2023-09-07 18:34:34 +0900

    📝 서버 테스트

--- IRE/src/App.js
+++ IRE/src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
-import { ThemeProvider } from 'styled-components';
-import theme from './style/theme';
+import "./App.css";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme";
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import Test from "./test";
 
-const staticServerUrl = process.env.REACT_APP_PATH || '';
+const staticServerUrl = process.env.REACT_APP_PATH || "";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path={staticServerUrl + '/'} />
-          <Route path={staticServerUrl + '/research'} />
-          <Route path={staticServerUrl + '/campingList/all'} />
-          <Route path={staticServerUrl + '/campingList/jeju'} />
-          <Route path={staticServerUrl + '/campingList/seongwipo'} />
-          <Route path={staticServerUrl + '/campingDetail/:id'} />
+          <Route path={staticServerUrl + "/"} element={<Test />} />
+          <Route path={staticServerUrl + "/research"} />
+          <Route path={staticServerUrl + "/campingList/all"} />
+          <Route path={staticServerUrl + "/campingList/jeju"} />
+          <Route path={staticServerUrl + "/campingList/seongwipo"} />
+          <Route path={staticServerUrl + "/campingDetail/:id"} />
         </Routes>
       </div>
     </ThemeProvider>
