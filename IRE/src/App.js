ba8497c9c045101f0dbdc408d154a0993ab2abb5
y00eunji <aine0227@naver.com>
2023-09-07 17:44:00 +0900

    style: ThemeProvider 세팅

--- IRE/src/App.js
+++ IRE/src/App.js
@@ -1,20 +1,23 @@
-import "./App.css";
-import Test from "./pages/test";
-import { Routes, Route } from "react-router-dom";
+import './App.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './style/theme';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Test />
-      <Routes>
-        <Route path="/" />
-        <Route path="/research" />
-        <Route path="/campingList/all" />
-        <Route path="/campingList/jeju" />
-        <Route path="/campingList/seongwipo" />
-        <Route path="/campingDetail/:id" />
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Routes>
+          <Route path='/' />
+          <Route path='/research' />
+          <Route path='/campingList/all' />
+          <Route path='/campingList/jeju' />
+          <Route path='/campingList/seongwipo' />
+          <Route path='/campingDetail/:id' />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
