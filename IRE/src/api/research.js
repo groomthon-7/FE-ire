a4e7b322f2e4b69cf0febcbd0a497f67fb9bd0de
LJH <049494@ewhain.net>
2023-09-08 10:16:02 +0900

    test

--- IRE/src/api/research.js
+++ IRE/src/api/research.js
@@ -3,7 +3,7 @@ import client from "./client";
 export const POST_Research = async (setIsLoading, isLoading, tag) => {
   try {
     setIsLoading(true);
-    const res = await client.get("/ques/gettag", { tag: tag });
+    const res = await client.get("/ques/gettag", { params: { tag: tag } });
     setIsLoading(false);
     return res.data;
   } catch (err) {
